169152cf9c384b8560fa4c110eac5e929009a079
slamensd <58567346+slamensd@users.noreply.github.com>
2023-04-10 19:40:50 +0000

    go

--- app.js
+++ app.js
@@ -1,152 +1,58 @@
-// Wait for the document to load before running the code
-document.addEventListener("DOMContentLoaded", () => {
-    // Initialize the provider
-    let provider;
-  
-    // Initialize the ERC1155 contract address and ABI
-    const contractAddress = "0x232765be70a5f0b49e2d72eee9765813894c1fc4";
-    const contractABI = [
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_id",
-            "type": "uint256"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_value",
-            "type": "uint256"
-          }
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256[]",
-            "name": "_ids",
-            "type": "uint256[]"
-          },
-          {
-            "internalType": "uint256[]",
-            "name": "_values",
-            "type": "uint256[]"
-          }
-        ],
-        "name": "safeBatchTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-      },
-      {
-        "inputs": [
-          {
-            "internalType": "address",
-            "name": "_owner",
-            "type": "address"
-          },
-          {
-            "internalType": "uint256",
-            "name": "_id",
-            "type": "uint256"
-          }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-          {
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-      }
-    ];
-  
-    // Initialize the Web3 object
-    const web3 = new Web3(window.ethereum);
-  
-    // Get the connect button
-    const connectButton = document.getElementById("connect-button");
-  
-    // Get the token count paragraph
-    const tokenCountParagraph = document.getElementById("token-count");
-  
-    // Get the burn button
-    const burnButton = document.getElementById("burn-button");
-  
-    // Get the hidden message paragraph
-    const hiddenMessageParagraph = document.getElementById("hidden-message");
-  
-    // Add a click event listener to the connect button
-    connectButton.addEventListener("click", async () => {
-      try {
-        // Detect the MetaMask provider
-        provider = await detectEthereumProvider();
-  
-        // If the provider is not detected, show an error message
-        if (!provider) {
-          throw new Error("Please install MetaMask to use this dApp");
-        }
-  
-        // Request access to the user's MetaMask wallet
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-  
-        // Set the Web3 provider to MetaMask
-        web3.setProvider(window.ethereum);
-  
-        // Get the user's Ethereum address
-        const accounts = await web3.eth.getAccounts();
-        const account = accounts[0];
-  
-        // Get the user's token count
-        const contract = new web3.eth.Contract(contractABI, contractAddress);
-        const tokenCount = await contract.methods.balanceOf(account, 6).call();
-              // Display the user's token count
-      tokenCountParagraph.innerText = `You have ${tokenCount} tokens of ID 6`;
+// Initialize Web3
+if (window.ethereum) {
+  window.web3 = new Web3(window.ethereum);
+  try {
+    // Request account access if needed
+    window.ethereum.enable();
+  } catch (error) {
+    console.error(error);
+  }
+} else if (window.web3) {
+  // Legacy dapp browsers...
+  window.web3 = new Web3(web3.currentProvider);
+} else {
+  // Non-dapp browsers...
+  console.error('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
 
-      // Show the burn button
-      burnButton.style.display = "block";
-    } catch (error) {
-      console.error(error);
-      alert(error.message);
-    }
-  });
+// Initialize ERC1155 contract
+const contractAddress = '0x232765be70a5f0b49e2d72eee9765813894c1fc4';
+const contractAbi = [{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"components":[{"internalType":"address","name":"diamondCutFacet","type":"address"},{"internalType":"address","name":"diamondLoupeFacet","type":"address"},{"internalType":"address","name":"erc165Facet","type":"address"},{"internalType":"address","name":"erc173Facet","type":"address"}],"internalType":"struct Diamond.CoreFacets","name":"_coreFacets","type":"tuple"},{"components":[{"internalType":"address","name":"facetAddress","type":"address"},{"internalType":"enum IDiamondCut.FacetCutAction","name":"action","type":"uint8"},{"internalType":"bytes4[]","name":"functionSelectors","type":"bytes4[]"}],"internalType":"struct IDiamondCut.FacetCut[]","name":"_facets","type":"tuple[]"},{"components":[{"internalType":"address","name":"initContract","type":"address"},{"internalType":"bytes","name":"initData","type":"bytes"}],"internalType":"struct Diamond.Initialization[]","name":"_initializations","type":"tuple[]"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"facet","type":"address"},{"internalType":"bytes4","name":"selector","type":"bytes4"}],"name":"ErrDiamondFacetAlreadyExists","type":"error"},{"stateMutability":"payable","type":"fallback"},{"inputs":[{"internalType":"bytes[]","name":"data","type":"bytes[]"}],"name":"multicall","outputs":[{"internalType":"bytes[]","name":"results","type":"bytes[]"}],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}];
+const contract = new web3.eth.Contract(contractAbi, contractAddress);
 
-  // Add a click event listener to the burn button
-burnButton.addEventListener("click", async () => {
-    try {
-      // Get the user's Ethereum address
-      const accounts = await web3.eth.getAccounts();
-      const account = accounts[0];
-  
-      // Burn the token
-      const contract = new web3.eth.Contract(contractABI, contractAddress);
-      await contract.methods.safeTransferFrom(account, "0x0000000000000000000000000000000000000000", 6, 1, "").send({ from: account });
-  
-      // Display the hidden message
-      hiddenMessageParagraph.style.display = "block";
-    } catch (error) {
-      console.error(error);
-      alert(error.message);
-    }
-  });
-  
-  
+// Connect wallet form submit event listener
+const connectWalletForm = document.getElementById('connect-wallet-form');
+connectWalletForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const walletAddressInput = document.getElementById('wallet-address-input');
+  const walletAddress = walletAddressInput.value;
+  const tokenBalance = await contract.methods.balanceOf(walletAddress, '0x0000000000000000000000000000000000000000000000000000000000000001').call();
+  if (tokenBalance > 0) {
+    document.getElementById('token-balance-span').textContent = tokenBalance;
+    document.getElementById('token-balance-div').style.display = 'block';
+  } else {
+    alert('You do not own any ERC1155 tokens.');
+  }
 });
-  
+
+// Burn token button click event listener
+const burnTokenButton = document.getElementById('burn-token-button');
+burnTokenButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  const walletAddressInput = document.getElementById('wallet-address-input');
+  const walletAddress = walletAddressInput.value;
+  const tokenBalance = await contract.methods.balanceOf(walletAddress, '0x0000000000000000000000000000000000000000000000000000000000000001').call();
+  if (tokenBalance > 0) {
+    const burnAmount = prompt('Enter the number of tokens to burn:');
+    if (burnAmount) {
+      const approveResult = await contract.methods.approve('0x000000000000000000000000000000000000dEaD', burnAmount).send({ from: walletAddress });
+      const burnResult = await contract.methods.burn('0x0000000000000000000000000000000000000000000000000000000000000001', burnAmount).send({ from: walletAddress });
+      if (burnResult.status) {
+        document.getElementById('hidden-message-div').style.display = 'block';
+        burnTokenButton.style.display = 'none';
+      }
+    }
+  } else {
+    alert('You do not own any ERC1155 tokens.');
+  }
+});
