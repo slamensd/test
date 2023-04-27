693e9dd33d6460233231da87cb8e6058abf837b9
slamensd <58567346+slamensd@users.noreply.github.com>
2023-04-27 20:24:01 +0000

    vv

--- app.js
+++ app.js
@@ -133,19 +133,20 @@ const ABI = [
 let web3, accounts, contract;
 
 async function initWeb3() {
-  if (window.ethereum) {
-    web3 = new Web3(window.ethereum);
-    try {
-      await window.ethereum.enable();
-    } catch (error) {
-      console.error("User denied account access");
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        web3 = new Web3(window.ethereum);
+      } catch (error) {
+        console.error("User denied account access");
+      }
+    } else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.error("No web3 provider detected");
     }
-  } else if (window.web3) {
-    web3 = new Web3(window.web3.currentProvider);
-  } else {
-    console.error("No web3 provider detected");
   }
-}
+  
 
 async function getAvailableNFTs() {
   const nftList = document.getElementById("nftList");
