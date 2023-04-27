6cae1b233acf3829f638760b4b687d14ac67155f
slamensd <58567346+slamensd@users.noreply.github.com>
2023-04-27 02:24:54 +0000

    go

--- app.js
+++ app.js
@@ -1,110 +1,12 @@
-const contractAddress = "0x9674739124d69d555712a30e0a44de648f494219";
-const abiUrl = "https://api.etherscan.io/api?module=contract&action=getabi&address=0x9674739124d69d555712a30e0a44de648f494219";
+const CONTRACT_ADDRESS = "0xfD9B9b6F42B3bC85F8078Be7636554E862A82316";
+const USDC_CONTRACT_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
-let web3;
-let contract;
+const ABI = [{"constant":false,"inputs":[{"name":"newImplementation","type":"address"}],"name":"upgradeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newImplementation","type":"address"},{"name":"data","type":"bytes"}],"name":"upgradeToAndCall","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"implementation","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newAdmin","type":"address"}],"name":"changeAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"admin","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_implementation","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":false,"name":"previousAdmin","type":"address"},{"indexed":false,"name":"newAdmin","type":"address"}],"name":"AdminChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"implementation","type":"address"}],"name":"Upgraded","type":"event"}];
 
-async function getOwnerTokenIds(ownerAddress) {
-    const tokenIds = [];
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
-    try {
-        const totalTokens = await contract.methods.totalSupply().call();
-        for (let i = 0; i < totalTokens; i++) {
-            const tokenId = await contract.methods.tokenByIndex(i).call();
-            const tokenOwner = await contract.methods.ownerOf(tokenId).call();
-            if (tokenOwner.toLowerCase() === ownerAddress.toLowerCase()) {
-                tokenIds.push(tokenId);
-            }
-        }
-    } catch (error) {
-        console.error("Error fetching token IDs:", error);
-    }
+const contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
 
-    return tokenIds;
-}
-  
-
-
-async function connectWallet() {
-    if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-        try {
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const accounts = await web3.eth.getAccounts();
-            const walletAddress = accounts[0];
-            document.getElementById('wallet-address').textContent = walletAddress;
-            document.getElementById('wallet-connect').style.display = 'none';
-            document.getElementById('dashboard').style.display = 'block';
-            
-            await getContractABI();
-            await displayERC721Balance(walletAddress);
-            const tokenIds = await getOwnerTokenIds(walletAddress);
-            await displayNFTs(tokenIds);
-        } catch (error) {
-            console.error("Error connecting wallet:", error);
-        }
-    } else {
-        alert("Please install MetaMask or another compatible wallet provider!");
-    }
-}
-
-async function getContractABI() {
-    try {
-        const response = await fetch(abiUrl);
-        const data = await response.json();
-        const abi = JSON.parse(data.result);
-        contract = new web3.eth.Contract(abi, contractAddress);
-    } catch (error) {
-        console.error("Error fetching ABI:", error);
-    }
-}
-
-async function displayERC721Balance(address) {
-    try {
-        const balance = await contract.methods.balanceOf(address).call();
-        document.getElementById('nippy-balance').textContent = balance;
-    } catch (error) {
-        console.error("Error fetching balance:", error);
-    }
-}
-
-async function displayNFTs(tokenIds) {
-    const nftGrid = document.getElementById('nft-grid');
-    const loader = document.getElementById('loader');
-    const ipfsGateway = 'https://ipfs.io/ipfs/';
-    nftGrid.innerHTML = '';
-    loader.style.display = 'block'; // Show loader
-
-    for (const tokenId of tokenIds) {
-        const tokenURI = await contract.methods.tokenURI(tokenId).call();
-        const ipfsUrl = tokenURI.replace('ipfs://', ipfsGateway);
-        const response = await fetch(ipfsUrl);
-        const metadata = await response.json();
-
-        const nft = document.createElement('div');
-        nft.className = 'nft';
-
-        const img = document.createElement('img');
-        img.src = metadata.image;
-        nft.appendChild(img);
-
-        const name = document.createElement('h3');
-        name.textContent = metadata.name;
-        nft.appendChild(name);
-
-        const traits = ['Character', 'Books', 'Videos', 'Games', 'Toys', 'Music'];
-
-        for (const trait of traits) {
-            const attribute = metadata.attributes.find(attr => attr.trait_type === trait);
-            if (attribute) {
-                const traitElement = document.createElement('p');
-                traitElement.textContent = `${trait}: ${attribute.value}`;
-                nft.appendChild(traitElement);
-            }
-        }
-
-        nftGrid.appendChild(nft);
-    }
-    loader.style.display = 'none'; // Hide loader
-}
-
+const connectWalletButton = document.getElementById("connect-wallet");
+const claimForm = document.querySelector(".claim-form");
+const claimUs
